3e5df7a20cbcf5f8a01397c0d24f48efe0866d74
Andrew Witmer <witmer.a@gmail.com>
2023-05-28 18:32:38 -0700

    Created intitial section styling + divisions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import data from "./data/data.json"
+import React, { useState } from 'react';
+
 
 function App() {
+  console.log(data);
+  
+  // something about this part is incorrect - needs to be fixed //
+  const [allProducts, setAllProducts] = useState(data)
+
   return (
+    <>
+    <div className="header">
+      <h1>Product Page</h1>
+    </div>
     <div>
       <h1>Hello, Student!</h1>
     </div>
+    <div className="footer">
+      <h6>Created using React</h6>
+    </div>            
+    </>
   );
 }
 
